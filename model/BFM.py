0977a01af1cb1980786dff234c5d101ae7fa3339
Abdelrahman350 <eng.abdelrahman.ahmad@gmail.com>
2021-11-18 13:34:53 +0200

    init changes

--- model/BFM.py
+++ model/BFM.py
@@ -8,31 +8,33 @@ class PCA(Layer):
         super(PCA, self).__init__()
         self.num_landmarks = num_landmarks
         self.pca_dir = pca_dir
-        self.w_shp = 0
-        self.w_exp = 0
-        self.w_tex = 0
-        self.u_shp = 0
-        self.u_exp = 0
-        self.u_tex = 0
-        self.keypoints = 0
+        self.u_base = 0
+        self.w_exp_base = 0
+        self.w_shp_base = 0
     
     def build(self):
-        self.w_shp = self.parsing_npy('w_shp_sim.npy')
-        self.w_exp = self.parsing_npy('w_exp_sim.npy')
-        self.w_tex = self.parsing_npy('w_tex_sim.npy')
-        self.u_shp = self.parsing_npy('u_shp_sim.npy')
-        self.u_exp = self.parsing_npy('u_exp_sim.npy')
-        self.u_tex = self.parsing_npy('u_tex_sim.npy')
-        self.keypoints = self.parsing_npy('keypoints_sim.npy')
-        self.param_mean = self.parsing_pkl('param_whitening.pkl').get('param_mean')
-        self.param_std = self.parsing_pkl('param_whitening.pkl').get('param_std')
+        w_exp = self.parsing_npy('w_exp_sim.npy')
+        w_shp = self.parsing_npy('w_shp_sim.npy')
+        w_tex = self.parsing_npy('w_tex_sim.npy')
+        u_exp = self.parsing_npy('u_exp.npy')
+        u_shp = self.parsing_npy('u_shp.npy')
+        u_tex = self.parsing_npy('u_tex.npy')
+        keypoints = self.parsing_npy('keypoints_sim.npy')
+        param_mean = self.parsing_pkl('param_whitening.pkl').get('param_mean')
+        param_std = self.parsing_pkl('param_whitening.pkl').get('param_std')
+        u = u_exp + u_shp
+        self.u_base = self.convert_npy_to_tensor(u[keypoints])
+        self.w_exp_base = self.convert_npy_to_tensor(w_exp[keypoints])
+        self.w_shp_base = self.convert_npy_to_tensor(w_shp[keypoints])
 
-    def forward(self):
+    def forward(self, pose_para, alpha_exp, alpha_shp):
         pass
 
     def parsing_npy(self, file):
-        npy_array = np.load(self.pca_dir+file)
-        return tf.convert_to_tensor(npy_array, dtype=tf.float32)
+        return np.load(self.pca_dir+file)
     
     def parsing_pkl(self, file):
         return pickle.load(open(self.pca_dir+file, 'rb'))
+    
+    def convert_npy_to_tensor(self, npy_array):
+        return tf.convert_to_tensor(npy_array, dtype=tf.float32)
