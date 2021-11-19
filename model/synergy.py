15053cb5728db6400449ed3cfb21f54fddbc6021
Abdelrahman350 <eng.abdelrahman.ahmad@gmail.com>
2021-11-19 23:15:17 +0200

    fixing typo

--- model/synergy.py
+++ model/synergy.py
@@ -14,12 +14,14 @@ from tensorflow.keras.layers import Input, GlobalAveragePooling2D, Dense, Dropou
 def create_synergy(input_shape, classes=62):
     inputs = Input(shape=input_shape)
     X = create_MobileNetV2(input_shape=input_shape, classes=classes)(inputs)
-    Z = GlobalAveragePooling2D(name='Global_Avg_Polling')(X)
+    Z = GlobalAveragePooling2D(name='Global_Avg_Pooling')(X)
     X_p = Dropout(0.2)(X)
     pose = Dense(name='pose', units=12)(X_p)
     X_exp = Dropout(0.2)(X)
     expression = Dense(name='expression', units=10)(X_exp)
     X_shape = Dropout(0.2)(X)
     shape = Dense(name='shape', units=40)(X_shape)
+
+
     model = Model(inputs=[inputs], outputs=[Z, pose, expression, shape], name='Synergy')
     return model
