3cd2b95e26beac56713139a41c039b40a75363c2
Abdelrahman350 <eng.abdelrahman.ahmad@gmail.com>
2021-12-15 12:34:36 +0200

    fixing data_loader

--- utils/loading_data.py
+++ utils/loading_data.py
@@ -2,20 +2,20 @@ import json
 from data_generator.data_generator import DataGenerator
 
 def loading_dictionaries(dataset='300w'):
-    base_dir_ids = "../../Datasets/partition_"
+    base_dir_ids = "../../Datasets/IDs_"
     base_dir_labels = "../../Datasets/labels_"
     json_file_ids = json_file_labels = 'None'
     if dataset=='300w':
-        json_file_ids = base_dir_ids + 'LP.json'
-        json_file_labels = base_dir_labels + 'LP.json'
+        json_file_ids = base_dir_ids + '300W_LP.json'
+        json_file_labels = base_dir_labels + '300W_LP.json'
     elif dataset=='AFLW':
-        json_file_ids = base_dir_ids + 'AFLW.json'
-        json_file_labels = base_dir_labels + 'AFLW.json'
+        json_file_ids = base_dir_ids + 'AFLW2000.json'
+        json_file_labels = base_dir_labels + 'AFLW2000.json'
     with open(json_file_ids, 'r') as j:
-        partition = json.loads(j.read())
+        IDs = json.loads(j.read())
     with open(json_file_labels, 'r') as j:
         labels = json.loads(j.read())
-    return partition, labels
+    return IDs, labels
 
 def loading_generators(dataset='300w', input_shape=(224, 224, 3), batch_size=16, shuffle=True):
     partition, labels = loading_dictionaries(dataset=dataset)
