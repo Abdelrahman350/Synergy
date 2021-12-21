3a9bb098fa6c57069cf3a1f39b3cc000e9ef5785
Abdelrahman350 <eng.abdelrahman.ahmad@gmail.com>
2021-12-21 19:41:28 +0200

    fix_6

--- data_generator/data_generator.py
+++ data_generator/data_generator.py
@@ -57,7 +57,7 @@ class DataGenerator(Sequence):
         X = np.array(X)
         batch_parameters_3DMM = np.array(batch_parameters_3DMM)
         batch_parameters_3DMM_hat = batch_parameters_3DMM.copy()
-        return X, (batch_parameters_3DMM, batch_parameters_3DMM)
+        return X, (batch_parameters_3DMM, batch_parameters_3DMM_hat)
 
     def get_one_instance(self, id):
         batch = [id]
