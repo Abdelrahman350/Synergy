00d07b89f56286fe728d6b4c8986e9b2f2213539
Abdelrahman350 <eng.abdelrahman.ahmad@gmail.com>
2021-12-26 14:01:03 +0200

    py3.8

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ gpus = tf.config.list_physical_devices('GPU')
 if gpus:
   # Restrict TensorFlow to only use the first GPU
   try:
-    tf.config.set_visible_devices(gpus[1], 'GPU')
+    tf.config.set_visible_devices(gpus[0], 'GPU')
     logical_gpus = tf.config.list_logical_devices('GPU')
     print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
   except RuntimeError as e:
