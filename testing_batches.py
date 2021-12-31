0df354be0354a456599a987c18fe8d8f80db1583
Abdelrahman350 <eng.abdelrahman.ahmad@gmail.com>
2021-12-31 03:01:20 +0200

    fixing

--- testing_batches.py
+++ testing_batches.py
@@ -16,7 +16,7 @@ if gpus:
     # Visible devices must be set before GPUs have been initialized
     print(e)
 
-input_shape = (450, 450, 3)
+input_shape = (128, 128, 3)
 training_data_generator, validation_data_generator = loading_generators(dataset='300w',\
       input_shape=input_shape, batch_size=32, shuffle=True)
 
@@ -25,18 +25,18 @@ list_ids = ["300W-LP/300W_LP/AFW/AFW_134212_1_2", "300W-LP/300W_LP/HELEN_Flip/HE
       "300W-LP/300W_LP/LFPW_Flip/LFPW_image_train_0047_4"]
 images, y = training_data_generator.data_generation(list_ids)
 
-DMM = y[0]
-poses = DMM
+y_DMM = y['Pm']
+poses_gt = y_DMM
 
 pca = PCA(input_shape)
-vertices_tf = pca(DMM)
+vertices_tf = pca(y_DMM)
 vertices = vertices_tf.numpy()
 
 for i in range(len(list_ids)):
   plot_landmarks(images[i], vertices[i], 'lmk_'+str(i))
 
 for i in range(len(list_ids)):
-  plot_pose(images[i], poses[i], 'pose_'+str(i))
+  plot_pose(images[i], poses_gt[i], 'pose_'+str(i))
 
 model = Synergy(input_shape=input_shape)
 model.summary()
