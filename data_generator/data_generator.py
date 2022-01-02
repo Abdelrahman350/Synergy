c33d6f73c72e383b5a0b552fb1e9e68918806504
Abdelrahman350 <eng.abdelrahman.ahmad@gmail.com>
2022-01-02 14:28:50 +0200

    add cropp

--- data_generator/data_generator.py
+++ data_generator/data_generator.py
@@ -41,7 +41,7 @@ class DataGenerator(Sequence):
         for index, image_id in enumerate(batch):
             image, aspect_ratio = image_loader(image_id, self.dataset_path, self.input_shape)
             parameters_3DMM = label_loader(image_id, self.labels, aspect_ratio)
-            lmks = self.pca(parameters_3DMM)
+            lmks = self.pca(np.expand_dims(parameters_3DMM, 0))
             image = augment(image, lmks, self.input_shape)
             X.append(image)
             batch_parameters_3DMM.append(parameters_3DMM)
