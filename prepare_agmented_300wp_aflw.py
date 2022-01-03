a9cb7b44911928d3c70f36ba6f1740ce16a6e6d1
Abdelrahman350 <eng.abdelrahman.ahmad@gmail.com>
2022-01-03 16:39:28 +0200

    preparing labels

--- prepare_agmented_300wp_aflw.py
+++ prepare_agmented_300wp_aflw.py
@@ -1,6 +1,7 @@
 from utils.data_utils.data_preparing_utils import dictionary_to_json
-from utils.loading_data_augmented import get_IDs
+from utils.loading_data_augmented import get_IDs, get_labels
 
 IDs = get_IDs()
-
-dictionary_to_json(IDs, '../../Datasets/300W_AFLW_Augmented/IDs_augmented.json')
+labels = get_labels()
+dictionary_to_json(IDs, '../../Datasets/300W_AFLW_Augmented/IDs_augmented')
+dictionary_to_json(labels, '../../Datasets/300W_AFLW_Augmented/labels_augmented')
--- utils/loading_data_augmented.py
+++ utils/loading_data_augmented.py
@@ -15,9 +15,12 @@ def get_IDs():
     dictionary['valid'] = list_files
     return dictionary
 
-def get_labels(list_files):
-    labels = {}
-    labels_list = np.array(load('../../Datasets/300W_AFLW_Augmented/3dmm_data/param_all_norm_v201.pkl'))
+def get_labels():
+    list_files_path = '../../Datasets/300W_AFLW_Augmented/3dmm_data/train_aug_120x120.list.train'
+    list_files = Path(list_files_path).read_text().strip().split('\n')
+    labels_list = np.array(load('../../Datasets/300W_AFLW_Augmented/3dmm_data/param_all_norm_v201.pkl'))[:, :62]
+    labels = dict(zip(list_files, labels_list))
+    return labels
     
 def get_suffix(filename):
     """a.jpg -> jpg"""
