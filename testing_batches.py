7af49860fd94ebc89bd8246534b5b6b076dfa95c
Abdelrahman350 <eng.abdelrahman.ahmad@gmail.com>
2022-01-05 10:42:52 +0200

    updating testing batches

--- testing_batches.py
+++ testing_batches.py
@@ -17,14 +17,22 @@ if gpus:
     # Visible devices must be set before GPUs have been initialized
     print(e)
 
-input_shape = (128, 128, 3)
-training_data_generator, validation_data_generator = loading_generators(dataset='300w',\
-      input_shape=input_shape, batch_size=32, shuffle=True)
+test = '300w'
+IMG_H = 128
 
-list_ids = ["300W-LP/300W_LP/AFW/AFW_134212_1_2", "300W-LP/300W_LP/HELEN_Flip/HELEN_1269874180_1_0",\
-     "300W-LP/300W_LP/AFW/AFW_4512714865_1_3", "300W-LP/300W_LP/LFPW_Flip/LFPW_image_train_0737_13",
-      "300W-LP/300W_LP/LFPW_Flip/LFPW_image_train_0047_4"]
-images, y = training_data_generator.data_generation(list_ids)
+input_shape = (IMG_H, IMG_H, 3)
+if test == 'AFLW':
+  training_data_generator, validation_data_generator = loading_generators(dataset='AFLW',\
+        input_shape=input_shape, batch_size=32, shuffle=True)
+  list_ids = ["AFLW2000-3D/AFLW2000/image00002", "AFLW2000-3D/AFLW2000/image00004",\
+    "AFLW2000-3D/AFLW2000/image00006", "AFLW2000-3D/AFLW2000/image00008",
+    "AFLW2000-3D/AFLW2000/image00010"]
+elif test == '300w':
+  training_data_generator, validation_data_generator = loading_generators(dataset='300w',\
+        input_shape=input_shape, batch_size=32, shuffle=True)
+  list_ids = ["300W-LP/300W_LP/AFW/AFW_134212_1_2", "300W-LP/300W_LP/HELEN_Flip/HELEN_1269874180_1_0",\
+      "300W-LP/300W_LP/AFW/AFW_4512714865_1_3", "300W-LP/300W_LP/LFPW_Flip/LFPW_image_train_0737_13",
+        "300W-LP/300W_LP/LFPW_Flip/LFPW_image_train_0047_4"]
 
 images_ori = []
 dataset_path='../../Datasets/300W_AFLW/'
