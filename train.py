3742092aa4f55ea952fb6d1fe5075ba95fa2ddc0
Abdelrahman350 <eng.abdelrahman.ahmad@gmail.com>
2022-01-24 11:42:03 +0200

    add metrics

--- train.py
+++ train.py
@@ -1,14 +1,15 @@
+from metrics import OrientationMAE
 from model.synergy import Synergy
 from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, CSVLogger
 from losses import ParameterLoss, WingLoss
 from set_tensorflow_configs import set_GPU
 from utils.loading_data import loading_generators
-from tensorflow.keras.optimizers import Adam, Nadam
+from tensorflow.keras.optimizers import Adam, Nadam, SGD
 import os
 from os import path
 
 set_GPU()
-dataset = 'DDFA'
+dataset = '300W_AFLW'
 param_loss = '_mse'
 IMG_H = 128
 input_shape = (IMG_H, IMG_H, 3)
@@ -23,7 +24,7 @@ training_data_generator, validation_data_generator = loading_generators(dataset=
 training_data_generator.augmentation = False
 
 model = Synergy(input_shape=input_shape, morphable=morphable)
-optimizer = Nadam(learning_rate=0.08)
+optimizer = SGD(learning_rate=0.08, momentum=0.9, nesterov=True)
 
 losses = {
   'Pm': ParameterLoss(name='loss_Param_In', mode='normal'),
@@ -33,7 +34,13 @@ losses = {
   }
 
 loss_weights = {'Pm':0.02, 'Pm*':0.02, 'Lc':0.05, 'Lr':0.05}
-model.compile(optimizer, losses, loss_weights=loss_weights)
+metrics = {'Pm': [
+  OrientationMAE(mode='pitch', name='pitch'), 
+  OrientationMAE(mode='yaw', name='yaw'), 
+  OrientationMAE(mode='roll', name='roll'), 
+  OrientationMAE(mode='avg', name='avg')
+  ]}
+model.compile(optimizer, losses, loss_weights=loss_weights, metrics=metrics)
 
 if load_model:
    model.load_weights(model_path)
