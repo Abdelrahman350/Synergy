9009eb11254eaaf49936c28b8d785cbaa45fffab
Abdelrahman350 <eng.abdelrahman.ahmad@gmail.com>
2022-02-03 12:21:00 +0200

    typo

--- model/backbone.py
+++ model/backbone.py
@@ -1,6 +1,6 @@
-from turtle import back
 from tensorflow.keras.applications import MobileNetV2
 
 def create_MobileNetV2(input_shape, include_top=False, classes=62):
-     backbone = MobileNetV2(input_shape=input_shape, include_top=include_top, weights='imagenet')
+     backbone = MobileNetV2(input_shape=input_shape, include_top=include_top, classes=classes,\
+           weights='imagenet')
      return backbone
