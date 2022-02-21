b513631568ef32211d17e3f1d99a6e544d0bc0d1
Abdelrahman350 <eng.abdelrahman.ahmad@gmail.com>
2022-02-21 11:20:53 +0200

    new backbones

--- model/backbone.py
+++ model/backbone.py
@@ -1,7 +1,14 @@
-from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.applications import MobileNetV2, EfficientNetB0
+from tensorflow.keras.applications.efficientnet import EfficientNetV2B0
 
 def create_backbone(input_shape, backbone='mobileNetV2', weights='imagenet', pooling=None):
      if backbone == 'mobileNetV2':
           backbone = MobileNetV2(input_shape=input_shape, include_top=False,\
+               weights=weights, pooling=pooling)
+     elif backbone == 'efficientNet':
+          backbone = EfficientNetB0(input_shape=input_shape, include_top=False,\
+               weights=weights, pooling=pooling)
+     elif backbone == 'efficientNetV2':
+          backbone = EfficientNetV2B0(input_shape=input_shape, include_top=False,\
                weights=weights, pooling=pooling)
      return backbone
